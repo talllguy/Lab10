c3a0a13d87a44833bbfe7010f58d8a661fda40ad
talllguy <elliott.plack@gmail.com>
2013-11-06 20:50:33 -0500

    Define problem

--- Lab10.cpp
+++ Lab10.cpp
@@ -2,6 +2,9 @@
 COSC 501
 Elliott Plack
 Lab 10
-6 NOV 2013
+6 NOV 2013                                                  Due 11 NOV 2013
+Problem:	Use a class to calculate price-change percentage of a given
+			stock, like Google (GOOG)
+Algorithm:	
 ***************************************************************************/
 
