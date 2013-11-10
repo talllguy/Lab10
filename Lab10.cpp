0b7ce7a8e355dca271b42b3b56e389012e4e5f75
talllguy <elliott.plack@gmail.com>
2013-11-10 17:22:10 -0500

    Getting there

--- Lab10.cpp
+++ Lab10.cpp
@@ -13,20 +13,45 @@ Algorithm:
 using namespace std;
 
 
-int main()
+class Stock
 {
-	class Stock
+	string symbol;
+	string name;
+	int previousClosingPrice;
+	int currentPrice;
+public:
+	Stock (string,string,double,double); // constructor
+	int readPrice(int,int);
+	float changePercent() {return ((currentPrice-previousClosingPrice)/previousClosingPrice);}
+};
+
+Stock::Stock(string a, string b,double c,double d)
 	{
-		string symbol();
-		string name();
-		double previousClosingPrice();
-		double currentPrice();
-	public:
-		Stock (string); // constructor
+		symbol = a;
+		name = b;
+		previousClosingPrice = c;
+		currentPrice = d;
+	}
+
+int main()
+	{
+		string symbol, name;
+		int previousClosingPrice, currentPrice;
+		
+		cout << "Enter the stock symbol: ";
+		cin >> symbol;
+		cout << "Enter the stock name: ";
+		cin >> name;
+		cout << "Enter the previous price: ";
+		cin >> previousClosingPrice;
+		cout << "Enter the current price: ";
+		cin >> currentPrice;
+
+		Stock go(symbol,name,previousClosingPrice,currentPrice);
 
 		
+		
+		return 0;
+	}
 
-	};
-
-
-}
+// example: class constructor
